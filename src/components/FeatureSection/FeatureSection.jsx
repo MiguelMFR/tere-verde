36cfe8da21b3f8289f226b7cbc50e0fc7c488961
coco-lucas <lucasgcocos@gmail.com>
2025-06-25 16:15:01 -0300

    feat: features com requisicoes na Home

--- src/components/FeatureSection/FeatureSection.jsx
+++ src/components/FeatureSection/FeatureSection.jsx
@@ -1,12 +1,12 @@
 import './FeatureSection.css';
 
-const FeatureSection = ({ title, description, image, reverse }) => {
+const FeatureSection = ({ title, description, image, reverse, onClick }) => {
   return (
     <section className={`feature-section ${reverse ? 'reverse' : ''}`}>
       <div className="feature-content">
         <h2>{title}</h2>
         <p>{description}</p>
-        <button className="btn">Saiba Mais</button>
+        <button className="btn" onClick={onClick}>Saiba Mais</button>
       </div>
       <div className="feature-image">
         <img src={image} alt={title} />
